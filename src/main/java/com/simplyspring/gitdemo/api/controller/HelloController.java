fa867048ecb7f44738e25364348c7f7de8199c45
unknown <sobhitakumarsahu@gmail.com>
2023-07-05 02:06:25 +0530

    added another method

--- src/main/java/com/simplyspring/gitdemo/api/controller/HelloController.java
+++ src/main/java/com/simplyspring/gitdemo/api/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.simplyspring.gitdemo.api.controller;
 import com.simplyspring.gitdemo.api.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +18,11 @@ public class HelloController {
     public String sayHello(){
         return helloService.greet();
     }
+
+    @GetMapping("/hello/{name}")
+    public String sayHelloWithName(@PathVariable String name){
+        return helloService.greetWithName(name);
+
+    }
+
 }
