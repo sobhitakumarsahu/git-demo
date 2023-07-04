ff5dbf75f16f0b1afa4288c7862c1e0cafdc980a
unknown <sobhitakumarsahu@gmail.com>
2023-07-05 01:46:10 +0530

    added controller code

---
+++ src/main/java/com/simplyspring/gitdemo/api/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.simplyspring.gitdemo.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String greet() {
+        return "Hello there";
+    }
+}
