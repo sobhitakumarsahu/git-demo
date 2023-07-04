7cb40bfd1d58624894f367074d64cf22dcbbce4e
unknown <sobhitakumarsahu@gmail.com>
2023-07-04 20:22:57 +0530

    added to local repo

---
+++ src/test/java/com/simplyspring/gitdemo/api/GitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplyspring.gitdemo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
